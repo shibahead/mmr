cbc29cb77b441a123e6b8030270fcf0e42aa9aca
Shibahead <shibahead@gmail.com>
2022-06-22 21:17:36 +0900

    Add files via upload

---
+++ readme.R
@@ -0,0 +1,35 @@
+    tagList(
+    wellPanel(
+      style="background:#0052A4;color:white"
+      ,h4("重要なご連絡")
+      ,"2022年7月1日より、MMRの利用にはユーザー登録が必要となります。"
+      ,"詳しくは、"
+      ,tags$a(style="color:white;text-decoration: underline","こちらのMacromillのTeams「MMRお知らせ」の記事",target="_blank",href="https://teams.microsoft.com/l/message/19:4617e83335cc4df39f51a7ed84c8f6a9@thread.skype/1655882447557?tenantId=644e05ec-c931-445c-8b08-599939d29a31&groupId=05701d53-109a-4aab-9e54-0e56a208701a&parentMessageId=1655882447557&teamName=MMR%EF%BC%88MacromillR%EF%BC%89%E3%81%8A%E7%9F%A5%E3%82%89%E3%81%9B&channelName=%E4%B8%80%E8%88%AC&createdTime=1655882447557")
+      ,"をご確認ください"
+    )
+    
+    ,wellPanel(
+      strong("MMRのご利用ありがとうございます！　※画面が縦に潰れているときは、ブラウザの幅を変えると戻ることがあります。")
+    )
+    ,wellPanel(
+      p(strong("このツールは何？"))
+      ,p("このツールはMMR（MacromillR）といいます。")
+      ,p("R言語の様々なパッケージを簡単に使えるようにツールにしたものです。")
+      ,p("Rのコードを書かずに、Rをインストールしていなくても、Rの便利で強力なデータ集計・分析機能を利用することができます。")
+    )
+    ,wellPanel(
+      p(strong("使い方"))
+      ,p("データ前処理では、データを結合・フィルタリングして集計解析しやすくすることができます。")
+      ,p("集計・可視化では、データを集計して、ピボットテーブルやグラフで視覚化することができます。")
+      ,p("データ解析では、データをアップロードして、様々な統計解析にかけることができます")
+      ,p("テキスト解析では、テキストを品詞に分解したり、ランキングやワードクラウドを作成できます。")
+      
+    )
+    ,wellPanel(
+      p(strong("連絡先"))
+      ,p("不具合・修正のご要望などはメール・Teamsまでお寄せいただけますと幸いです")
+      ,tags$a("開発担当者にメールを送る：池田拓郎・林潔・坂口雅哉",target="_blank",href="mailto:t_ikeda@macromill.com;j_lin@macromill.com;m_sakaguchi@macromill.com")
+      ,p()
+      ,tags$a("Teamsに投稿：MMR不具合・修正・要望チャンネル", target="_blank",href="https://teams.microsoft.com/l/channel/19%3acc424a5c86f2444f996881c2a3658385%40thread.skype/%25E8%25B3%25AA%25E5%2595%258F%25E3%2583%25BB%25E4%25B8%258D%25E5%2585%25B7%25E5%2590%2588%25E3%2583%25BB%25E8%25A6%2581%25E6%259C%259B?groupId=05701d53-109a-4aab-9e54-0e56a208701a&tenantId=644e05ec-c931-445c-8b08-599939d29a31")
+    )
+)
